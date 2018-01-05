3f2535eca94ece377bc955bef7424de9182f14d6
Vlad Matei <vladmatei139@gmail.com>
2018-01-05 15:03:40 +0200

    Added MBUser model

--- MovieBoxProject/MovieBox/admin.py
+++ MovieBoxProject/MovieBox/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from MovieBox.models import Movie, MBUser
+
+admin.site.register(Movie)
+admin.site.register(MBUser)
--- MovieBoxProject/MovieBox/models.py
+++ MovieBoxProject/MovieBox/models.py
@@ -3,7 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class MBUser(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	email = models.CharField(max_length = 100)
 
 class Movie(models.Model):
     name = models.CharField(max_length=100)
