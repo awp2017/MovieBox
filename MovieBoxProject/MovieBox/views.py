d6715812fe6ffd5613035446239379ccad3d1316
Vlad Matei <vladmatei139@gmail.com>
2018-01-05 15:36:16 +0200

    Added Front page view

--- MovieBoxProject/MovieBox/views.py
+++ MovieBoxProject/MovieBox/views.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.views.generic import ListView
+
 # Create your views here.
+
+class FrontPageMovieListView(ListView):
+	template_name = 'homeMovieList.html'
+	model = Movie
+	context_object_name = 'movies'
