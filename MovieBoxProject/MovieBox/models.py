1e502454d1e12f4d07125ab87a7261aeb47c74c2
daneel95 <holteiu.daniel@gmail.com>
2018-01-05 14:51:00 +0200

    added movie model

--- MovieBoxProject/MovieBox/models.py
+++ MovieBoxProject/MovieBox/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+    year = models.DateTimeField()
+    genre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
