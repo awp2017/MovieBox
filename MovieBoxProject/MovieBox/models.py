6f6da4994783aa4890c9a0dcc85b9809cff6b678
SoniaHorchidan <sonia.florina@yahoo.com>
2018-01-13 17:22:24 +0200

    added some css :) :) :)

--- MovieBoxProject/MovieBox/models.py
+++ MovieBoxProject/MovieBox/models.py
@@ -12,7 +12,7 @@ class Actor(models.Model):
     first_name = models.CharField(max_length = 100)
     last_name = models.CharField(max_length=100)
     birth_date = models.DateField(null=True, blank=True)
-    picture =  models.CharField(max_length=200, default='actor-defaul.jpg')
+    picture =  models.ImageField(default='', blank=True)
 
     def __str__(self):
         return self.last_name + " " + self.first_name
